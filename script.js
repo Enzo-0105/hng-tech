1cc0715e21ed264ce5b5aa549df091cf0e15c7d1
Ubuntu <ubuntu@ip-172-31-22-157.ec2.internal>
2024-06-27 21:36:21 +0000

    hng project

---
+++ script.js
@@ -0,0 +1,16 @@
+// Your personal information
+const personalInfo = {
+    name: "Salako lateef Olatunbosun",
+    username: "Olatunbosun",
+    email: "Salakolateef331@gmail.com"
+};
+
+// Function to display personal information
+function displayInfo() {
+    document.getElementById('name').innerText = personalInfo.name;
+    document.getElementById('username').innerText = personalInfo.username;
+    document.getElementById('email').innerText = personalInfo.email;
+}
+
+// Call the function to display information on page load
+displayInfo();
